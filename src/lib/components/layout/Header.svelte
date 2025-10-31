337f5dd79c6b30e6d87a607e90991c288ef311dc
Taylor Ren <taylor.ren@gmail.com>
2025-10-31 15:31:05 +0800

    rsywx.com will use https

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -157,7 +157,7 @@
 						</svg>
 					</a>
 					<a
-						href="http://rsywx.com"
+						href="https://rsywx.com"
 						target="_blank"
 						rel="noopener noreferrer"
 						class="inline-flex items-center gap-1 rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-white"
@@ -347,7 +347,7 @@
 						</svg>
 					</a>
 					<a
-						href="http://rsywx.com"
+						href="https://rsywx.com"
 						target="_blank"
 						rel="noopener noreferrer"
 						onclick={closeMobileMenu}
